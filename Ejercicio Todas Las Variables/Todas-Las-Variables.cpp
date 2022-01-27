1aedbcf7305833984bef4d2827347eb1163fcc5f
Jose Alvarenga <josedanialma001234@gmail.com>
2022-01-27 12:29:45 -0600

    Valor Todas Las Variables

---
+++ Ejercicio Todas Las Variables/Todas-Las-Variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char** argv) {
+
+bool valorBolean;
+valorBolean = true;
+
+int valorEntero = 15;
+double valorDouble = 20.99;
+string valorStirng = " Hola como estan ";
+char valorChar = 'B';
+
+cout << " Valor Boolean: " << valorBolean << endl;
+cout << " Valor Entero: " << valorEntero << endl;
+cout << " Valor Double: " << valorDouble << endl;
+cout << " Valor String: " << valorStirng << endl;
+cout << " Valor Char. " << valorChar << endl;
+
+return 0;
+}
